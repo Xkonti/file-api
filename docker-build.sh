654fc61e1c50813f30169f138a201d0eb1b6f921
Xkonti <xkonti@xkonti.tech>
2023-08-20 17:25:00 -0400

    Set up building and publishing the docker image to DockerHub (#3)

---
+++ docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e # Exit immediately if a command exits with a non-zero status
+
+# Extract version number from package.json
+version=$(grep '"version":' package.json | sed -E 's/.*"version": "([^"]+)".*/\1/')
+
+# Get individual components of the version
+major=$(echo $version | cut -d '.' -f 1)
+minor=$(echo $version | cut -d '.' -f 2)
+patch=$(echo $version | cut -d '.' -f 3)
+
+# Build Docker images with different tags
+docker build --pull -t xkonti/file-api:${major}.${minor}.${patch} .
+docker build --pull -t xkonti/file-api:${major}.${minor} .
+docker build --pull -t xkonti/file-api:${major} .
+docker build --pull -t xkonti/file-api:latest .
