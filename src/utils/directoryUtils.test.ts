67afa3c92e780a1d364e07c382de6ad834987189
Xkonti <xkonti@xkonti.tech>
2023-08-22 03:36:36 -0400

    Setup testing (#4)
    - [x] Create tests for the all `directoryUtils.ts` functions
    - [x] Create GitHub Action that runs tests for PRs
    - [x] Set test coverage threshold to 80% of lines and 85% of functions
    
    Additionally:
    - Add option to include directories in `flattenFiles` function

---
+++ src/utils/directoryUtils.test.ts
@@ -0,0 +1,118 @@
+import {describe, expect, test} from 'bun:test';
+import {
+  DirectoryEntry,
+  flattenFiles,
+  listSorterByPath,
+  listSorterByType,
+  readDirectoryContents,
+} from './directoryUtils';
+import {
+  getFakeTree,
+  testingDirectoryPath,
+  testingDirectoryRelativePath,
+  testingDirectoryTreeDepth1,
+  testingDirectoryTreeDepth2,
+  testingDirectoryTreeDepth3,
+} from './testingUtils.test';
+
+describe('readDirectoryContents()', () => {
+  test('should return proper depth 1 tree', async () => {
+    const actualTree = (await readDirectoryContents(
+      testingDirectoryPath,
+      testingDirectoryRelativePath,
+      1,
+    )) as DirectoryEntry[];
+    // Flatten and sort to make sure the contents are the same
+    const actualList = flattenFiles(actualTree, false).sort(listSorterByPath);
+    const expectedList = flattenFiles(testingDirectoryTreeDepth1, false).sort(listSorterByPath);
+    // Use only toEqual() as we don't care about the contents to be undefined. It isn't serialized to JSON.
+    expect(actualList).toEqual(expectedList);
+  });
+  test('should return proper depth 2 tree', async () => {
+    const actualTree = (await readDirectoryContents(
+      testingDirectoryPath,
+      testingDirectoryRelativePath,
+      2,
+    )) as DirectoryEntry[];
+    // Flatten and sort to make sure the contents are the same
+    const actualList = flattenFiles(actualTree, false).sort(listSorterByPath);
+    const expectedList = flattenFiles(testingDirectoryTreeDepth2, false).sort(listSorterByPath);
+    // Use only toEqual() as we don't care about the contents to be undefined. It isn't serialized to JSON.
+    expect(actualList).toEqual(expectedList);
+  });
+  test('should return proper depth 3 tree', async () => {
+    const actualTree = (await readDirectoryContents(
+      testingDirectoryPath,
+      testingDirectoryRelativePath,
+      3,
+    )) as DirectoryEntry[];
+    // Flatten and sort to make sure the contents are the same
+    const actualList = flattenFiles(actualTree, false).sort(listSorterByPath);
+    const expectedList = flattenFiles(testingDirectoryTreeDepth3, false).sort(listSorterByPath);
+    // Use only toEqual() as we don't care about the contents to be undefined. It isn't serialized to JSON.
+    expect(actualList).toEqual(expectedList);
+  });
+  test('should return "no-path" when invalid path', async () => {
+    const actualTree = await readDirectoryContents(`${testingDirectoryPath}/invalid/path`, '/', 1);
+    expect(actualTree).toBe('no-path');
+  });
+  test('should return "no-dir" when path to file', async () => {
+    const actualTree = await readDirectoryContents(`${testingDirectoryPath}/file1.empty`, '/', 1);
+    expect(actualTree).toBe('no-dir');
+  });
+});
+
+describe('flattenFiles()', () => {
+  test('including directories should return a list of only files', () => {
+    const {tree, flatList} = getFakeTree('', 3, 5, 5);
+    flatList.sort(listSorterByPath);
+    if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
+    const flattenedFiles = flattenFiles(tree, false).sort(listSorterByPath);
+    expect(flattenedFiles).toStrictEqual(flatList);
+  });
+
+  test('excluding directories should return a list of files/dirs', () => {
+    const {tree, flatListNoDirs} = getFakeTree('', 3, 5, 5);
+    flatListNoDirs.sort(listSorterByPath);
+    if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
+    const flattenedFiles = flattenFiles(tree, true).sort(listSorterByPath);
+    expect(flattenedFiles).toStrictEqual(flatListNoDirs);
+  });
+});
+
+describe('listSorters', () => {
+  test('listSorterByType - should sort directories before files', () => {
+    const entries: DirectoryEntry[] = [
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+    ];
+    const sortedEntries = entries.sort(listSorterByType);
+    expect(sortedEntries).toEqual([
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+    ]);
+  });
+  test('listSorterByPath - should sort purely by path', () => {
+    const entries: DirectoryEntry[] = [
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+    ];
+    const sortedEntries = entries.sort(listSorterByPath);
+    expect(sortedEntries).toEqual([
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+    ]);
+  });
+});
--- src/utils/directoryUtils.ts
+++ src/utils/directoryUtils.ts
@@ -1,61 +1,67 @@
-import { join } from "path";
-import { readdir } from "fs/promises";
-import { Dirent } from "fs";
+import {join} from 'path';
+import {readdir} from 'fs/promises';
+import {Dirent} from 'fs';
 
 export type DirectoryEntry = {
-    name: string,
-    fullPath: string,
-    type: 'dir' | 'file',
-    contents?: DirectoryEntry[],
-}
+  name: string;
+  fullPath: string;
+  type: 'dir' | 'file';
+  contents?: DirectoryEntry[];
+};
 
 /**
  * Reads the contents of a directory and returns a list of directory entries in a tree structure.
  * @param directoryPath Path to the directory to read
- * @param relativePath Relative path to the directory from the root of the data directory that the server is serving
+ * @param relativePath Relative path to the directory from the root of the data directory that the server is serving. This is used to generate the full path of the directory entries.
  * @param depth Depth to read the directory to. 1 means only the contents of the directory itself, 2 means the contents of the directory and the contents of the directories inside the directory, etc.
  * @returns Returns a list of directory entries, or a string if there was an error
  */
-export async function readDirectoryContents(directoryPath: string, relativePath: string, depth: number): Promise<DirectoryEntry[] | string> {
+export async function readDirectoryContents(
+  directoryPath: string,
+  relativePath: string,
+  depth: number,
+): Promise<DirectoryEntry[] | string> {
+  let rawEntries: Dirent[] = [];
 
-    let rawEntries: Dirent[] = [];
+  try {
+    rawEntries = await readdir(directoryPath, {withFileTypes: true});
+  } catch (error) {
+    const message = (error as {message: string}).message.toLowerCase();
+    console.error('Error when getting directory contents: ', message);
+    if (message.includes('no such file or directory')) {
+      return 'no-path';
+    } else if (message.includes('not a directory')) {
+      return 'no-dir';
+    }
+    return `error: ${message}`;
+  }
 
-    try {
-        rawEntries = await readdir(directoryPath, { withFileTypes: true });
-    } catch (error: { message: string }) {
-        console.error('Error when getting directory contents: ', error.message);
-        if (error.message.toLowerCase().includes('no such file or directory')) {
-            return 'no-path';
-        }
-        return `error: ${error.message}`;
+  const entries: DirectoryEntry[] = [];
+
+  for (const entry of rawEntries) {
+    const isDirectory = entry.isDirectory();
+    const fullPath = join(relativePath, entry.name);
+
+    // If the entry is a directory and we are not at the maximum depth
+    // then we need to recursively read the contents of the directory
+    let contents = undefined;
+    if (isDirectory && depth > 1) {
+      contents = await readDirectoryContents(join(directoryPath, entry.name), fullPath, depth - 1);
+      // If contents are of type string, then we have an error
+      if (typeof contents === 'string') {
+        return contents;
+      }
     }
 
-    const entries: DirectoryEntry[] = [];
+    entries.push({
+      name: entry.name,
+      fullPath: join(relativePath, entry.name),
+      type: isDirectory ? 'dir' : 'file',
+      contents,
+    });
+  }
 
-    for (const entry of rawEntries) {
-        const isDirectory = entry.isDirectory();
-        const fullPath = join(relativePath, entry.name);
-
-        // If the entry is a directory and we are not at the maximum depth
-        // then we need to recursively read the contents of the directory
-        let contents = undefined;
-        if (isDirectory && depth > 1) {
-            contents = await readDirectoryContents(join(directoryPath, entry.name), fullPath, depth - 1);
-            // If contents are of type string, then we have an error
-            if (typeof contents === 'string') {
-                return contents;
-            }
-        };
-
-        entries.push({
-            name: entry.name,
-            fullPath: join(relativePath, entry.name),
-            type: isDirectory ? 'dir' : 'file',
-            contents,
-        });
-    }
-
-    return entries;
+  return entries;
 }
 
 /**
@@ -63,13 +69,43 @@ export async function readDirectoryContents(directoryPath: string, relativePath:
  * @param entries List of directory & file entries
  * @returns Returns a list of file entries
  */
-export function flattenFiles(entries: DirectoryEntry[]): DirectoryEntry[] {
-    return entries.reduce((acc: DirectoryEntry[], curr: DirectoryEntry) => {
-        if (curr.type === 'file') {
-            return [...acc, curr];
-        } else if (curr.type === 'dir' && curr.contents) {
-            return [...acc, ...flattenFiles(curr.contents)];
-        }
-        return acc;
-    }, []);
-};
+export function flattenFiles(entries: DirectoryEntry[], excludeDirs: boolean): DirectoryEntry[] {
+  return entries.reduce((acc: DirectoryEntry[], curr: DirectoryEntry) => {
+    if (curr.type === 'file') {
+      return [...acc, curr];
+    } else if (curr.type === 'dir' && curr.contents) {
+      if (!excludeDirs) {
+        const currentNoContents = {
+          name: curr.name,
+          fullPath: curr.fullPath,
+          type: curr.type,
+        };
+        return [...acc, currentNoContents, ...flattenFiles(curr.contents, excludeDirs)];
+      }
+      return [...acc, ...flattenFiles(curr.contents, excludeDirs)];
+    }
+    return acc;
+  }, []);
+}
+
+/**
+ * Sorts a list of file/directory entries. Directories are sorted first, files are sorted second by full path.
+ */
+export function listSorterByType(a: DirectoryEntry, b: DirectoryEntry): number {
+  // Directories first
+  if (a.type === 'dir' && b.type === 'file') return -1;
+  if (a.type === 'file' && b.type === 'dir') return 1;
+
+  // Then sort by full path
+  return listSorterByPath(a, b);
+}
+
+/**
+ * Sorts a list of file/directory entries. Everything is sorted by full path.
+ */
+export function listSorterByPath(a: DirectoryEntry, b: DirectoryEntry): number {
+  // Then sort by full path
+  if (a.fullPath < b.fullPath) return -1;
+  if (a.fullPath > b.fullPath) return 1;
+  return 0;
+}
