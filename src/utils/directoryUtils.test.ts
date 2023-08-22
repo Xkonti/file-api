3542962499a62b4a7c8daa04ca38d9826cdc86ef
Xkonti <online.xkonti@gmail.com>
2023-08-22 02:30:18 -0400

    Group flattenFiles tests

--- src/utils/directoryUtils.test.ts
+++ src/utils/directoryUtils.test.ts
@@ -1,19 +1,21 @@
-import {expect, test} from 'bun:test';
+import {describe, expect, test} from 'bun:test';
 import {flattenFiles, listSorterByPath} from './directoryUtils';
 import {getFakeTree} from './testingUtils.test';
 
-test('Flattening a directory including directories should return a list of only files', () => {
-  const {tree, flatList} = getFakeTree('', 3, 5, 5);
-  flatList.sort(listSorterByPath);
-  if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
-  const flattenedFiles = flattenFiles(tree, false).sort(listSorterByPath);
-  expect(flattenedFiles).toStrictEqual(flatList);
-});
+describe('flattenFiles()', () => {
+  test('including directories should return a list of only files', () => {
+    const {tree, flatList} = getFakeTree('', 3, 5, 5);
+    flatList.sort(listSorterByPath);
+    if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
+    const flattenedFiles = flattenFiles(tree, false).sort(listSorterByPath);
+    expect(flattenedFiles).toStrictEqual(flatList);
+  });
 
-test('Flattening a directory excluding directories should return a list of files/dirs', () => {
-  const {tree, flatListNoDirs} = getFakeTree('', 3, 5, 5);
-  flatListNoDirs.sort(listSorterByPath);
-  if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
-  const flattenedFiles = flattenFiles(tree, true).sort(listSorterByPath);
-  expect(flattenedFiles).toStrictEqual(flatListNoDirs);
+  test('excluding directories should return a list of files/dirs', () => {
+    const {tree, flatListNoDirs} = getFakeTree('', 3, 5, 5);
+    flatListNoDirs.sort(listSorterByPath);
+    if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
+    const flattenedFiles = flattenFiles(tree, true).sort(listSorterByPath);
+    expect(flattenedFiles).toStrictEqual(flatListNoDirs);
+  });
 });
