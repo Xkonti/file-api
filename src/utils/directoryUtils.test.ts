c539b114b9ddc7f92d930456488a980131ab78cb
Xkonti <online.xkonti@gmail.com>
2023-08-22 03:11:31 -0400

    Test rest if directory utils

--- src/utils/directoryUtils.test.ts
+++ src/utils/directoryUtils.test.ts
@@ -1,5 +1,11 @@
 import {describe, expect, test} from 'bun:test';
-import {flattenFiles, listSorterByPath, readDirectoryContents} from './directoryUtils';
+import {
+  DirectoryEntry,
+  flattenFiles,
+  listSorterByPath,
+  listSorterByType,
+  readDirectoryContents,
+} from './directoryUtils';
 import {
   getFakeTree,
   testingDirectoryPath,
@@ -37,6 +43,14 @@ describe('readDirectoryContents()', () => {
     // Use only toEqual() as we don't care about the contents to be undefined. It isn't serialized to JSON.
     expect(actualTree).toEqual(testingDirectoryTreeDepth3);
   });
+  test('should return "no-path" when invalid path', async () => {
+    const actualTree = await readDirectoryContents(`${testingDirectoryPath}/invalid/path`, '/', 1);
+    expect(actualTree).toBe('no-path');
+  });
+  test('should return "no-dir" when path to file', async () => {
+    const actualTree = await readDirectoryContents(`${testingDirectoryPath}/file1.empty`, '/', 1);
+    expect(actualTree).toBe('no-dir');
+  });
 });
 
 describe('flattenFiles()', () => {
@@ -54,5 +68,42 @@ describe('flattenFiles()', () => {
     if (tree === undefined) throw new Error('Tree is undefined - testing data is broken');
     const flattenedFiles = flattenFiles(tree, true).sort(listSorterByPath);
     expect(flattenedFiles).toStrictEqual(flatListNoDirs);
+  });
+});
+
+describe('listSorters', () => {
+  test('listSorterByType - should sort directories before files', () => {
+    const entries: DirectoryEntry[] = [
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+    ];
+    const sortedEntries = entries.sort(listSorterByType);
+    expect(sortedEntries).toEqual([
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+    ]);
+  });
+  test('listSorterByPath - should sort purely by path', () => {
+    const entries: DirectoryEntry[] = [
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+    ];
+    const sortedEntries = entries.sort(listSorterByPath);
+    expect(sortedEntries).toEqual([
+      {name: 'dir1', type: 'dir', fullPath: '/dir1'},
+      {name: 'dir1/file2.txt', type: 'file', fullPath: '/dir1/file2.txt'},
+      {name: 'dir2', type: 'dir', fullPath: '/dir2'},
+      {name: 'dir2/file3.txt', type: 'file', fullPath: '/dir2/file3.txt'},
+      {name: 'file1.txt', type: 'file', fullPath: '/file1.txt'},
+    ]);
   });
 });
