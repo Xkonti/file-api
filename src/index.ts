b588f0079edaf69f08f5664ce1e97c0e0b9c1aa4
Xkonti <online.xkonti@gmail.com>
2023-08-22 00:51:39 -0400

    Configure prettier and reformat project

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,63 @@
-import { Elysia } from "elysia";
-import { join } from "path";
-import { flattenFiles, readDirectoryContents } from "./utils/directoryUtils";
+import {Elysia} from 'elysia';
+import {join} from 'path';
+import {flattenFiles, readDirectoryContents} from './utils/directoryUtils';
 
 const app = new Elysia()
-    // Verify API key before handling any requests
-    .onRequest(({ request }) => {
-        let apiKey = request.headers.get('apikey');
-        if (apiKey == null) {
-            apiKey = new URL(request.url).searchParams.get('apikey');
-        }
-        if (apiKey !== process.env.API_KEY) {
-            throw new Error('unauthorized');
-        }
-    })
+  // Verify API key before handling any requests
+  .onRequest(({request}) => {
+    let apiKey = request.headers.get('apikey');
+    if (apiKey == null) {
+      apiKey = new URL(request.url).searchParams.get('apikey');
+    }
+    if (apiKey !== process.env.API_KEY) {
+      throw new Error('unauthorized');
+    }
+  })
 
-    // Handle the unauthorized error
-    .onError(({ error, set }) => {
-        if (error.message === 'unauthorized') {
-            set.status = 401;
-            return 'Unauthorized';
-        }
-    })
+  // Handle the unauthorized error
+  .onError(({error, set}) => {
+    if (error.message === 'unauthorized') {
+      set.status = 401;
+      return 'Unauthorized';
+    }
+  })
 
-    .get("list", async ({ query, set }) => {
-        const relativePath = query.path ? query.path as string : null;
+  .get('list', async ({query, set}) => {
+    const relativePath = query.path ? (query.path as string) : null;
 
-        // TODO: Add proper checks for path validity
-        if (relativePath == null || relativePath.includes('..')) {
-            set.status = 400;
-            return "You must provide a valid path to a directory";
-        }
+    // TODO: Add proper checks for path validity
+    if (relativePath == null || relativePath.includes('..')) {
+      set.status = 400;
+      return 'You must provide a valid path to a directory';
+    }
 
-        const includeDirectories = query.dirs === 'true';
-        const depth = query.depth === undefined ? 1 : parseInt(query.depth as string);
-        const directoryPath = join(process.env.DATA_DIR as string, relativePath);
-      
-        const entries = await readDirectoryContents(directoryPath, relativePath, depth);
+    const includeDirectories = query.dirs === 'true';
+    const depth = query.depth === undefined ? 1 : parseInt(query.depth as string);
+    const directoryPath = join(process.env.DATA_DIR as string, relativePath);
 
-        // Handle the occurrence of an error
-        if (typeof entries === 'string') {
-            if (entries === 'no-path') {
-                set.status = 400;
-                return "You must provide a valid path to a directory";
-            }
+    const entries = await readDirectoryContents(directoryPath, relativePath, depth);
 
-            set.status = 500;
-            return 'An unknown error occurred';
-        }
+    // Handle the occurrence of an error
+    if (typeof entries === 'string') {
+      if (entries === 'no-path') {
+        set.status = 400;
+        return 'You must provide a valid path to a directory';
+      }
 
+      set.status = 500;
+      return 'An unknown error occurred';
+    }
 
-        // If we are not including directories, then we need to filter them out
-        // We still need to include the contents of the directories though
-        let result = entries;
-        if (!includeDirectories) {
-            result = flattenFiles(entries);
-        }
+    // If we are not including directories, then we need to filter them out
+    // We still need to include the contents of the directories though
+    let result = entries;
+    if (!includeDirectories) {
+      result = flattenFiles(entries);
+    }
 
-        return result;
-    })
+    return result;
+  })
 
-    .listen(3000);
+  .listen(3000);
 
-
-console.log(
-    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
--- src/utils/directoryUtils.ts
+++ src/utils/directoryUtils.ts
@@ -1,13 +1,13 @@
-import { join } from "path";
-import { readdir } from "fs/promises";
-import { Dirent } from "fs";
+import {join} from 'path';
+import {readdir} from 'fs/promises';
+import {Dirent} from 'fs';
 
 export type DirectoryEntry = {
-    name: string,
-    fullPath: string,
-    type: 'dir' | 'file',
-    contents?: DirectoryEntry[],
-}
+  name: string;
+  fullPath: string;
+  type: 'dir' | 'file';
+  contents?: DirectoryEntry[];
+};
 
 /**
  * Reads the contents of a directory and returns a list of directory entries in a tree structure.
@@ -16,46 +16,49 @@ export type DirectoryEntry = {
  * @param depth Depth to read the directory to. 1 means only the contents of the directory itself, 2 means the contents of the directory and the contents of the directories inside the directory, etc.
  * @returns Returns a list of directory entries, or a string if there was an error
  */
-export async function readDirectoryContents(directoryPath: string, relativePath: string, depth: number): Promise<DirectoryEntry[] | string> {
+export async function readDirectoryContents(
+  directoryPath: string,
+  relativePath: string,
+  depth: number,
+): Promise<DirectoryEntry[] | string> {
+  let rawEntries: Dirent[] = [];
 
-    let rawEntries: Dirent[] = [];
+  try {
+    rawEntries = await readdir(directoryPath, {withFileTypes: true});
+  } catch (error: {message: string}) {
+    console.error('Error when getting directory contents: ', error.message);
+    if (error.message.toLowerCase().includes('no such file or directory')) {
+      return 'no-path';
+    }
+    return `error: ${error.message}`;
+  }
 
-    try {
-        rawEntries = await readdir(directoryPath, { withFileTypes: true });
-    } catch (error: { message: string }) {
-        console.error('Error when getting directory contents: ', error.message);
-        if (error.message.toLowerCase().includes('no such file or directory')) {
-            return 'no-path';
-        }
-        return `error: ${error.message}`;
+  const entries: DirectoryEntry[] = [];
+
+  for (const entry of rawEntries) {
+    const isDirectory = entry.isDirectory();
+    const fullPath = join(relativePath, entry.name);
+
+    // If the entry is a directory and we are not at the maximum depth
+    // then we need to recursively read the contents of the directory
+    let contents = undefined;
+    if (isDirectory && depth > 1) {
+      contents = await readDirectoryContents(join(directoryPath, entry.name), fullPath, depth - 1);
+      // If contents are of type string, then we have an error
+      if (typeof contents === 'string') {
+        return contents;
+      }
     }
 
-    const entries: DirectoryEntry[] = [];
+    entries.push({
+      name: entry.name,
+      fullPath: join(relativePath, entry.name),
+      type: isDirectory ? 'dir' : 'file',
+      contents,
+    });
+  }
 
-    for (const entry of rawEntries) {
-        const isDirectory = entry.isDirectory();
-        const fullPath = join(relativePath, entry.name);
-
-        // If the entry is a directory and we are not at the maximum depth
-        // then we need to recursively read the contents of the directory
-        let contents = undefined;
-        if (isDirectory && depth > 1) {
-            contents = await readDirectoryContents(join(directoryPath, entry.name), fullPath, depth - 1);
-            // If contents are of type string, then we have an error
-            if (typeof contents === 'string') {
-                return contents;
-            }
-        };
-
-        entries.push({
-            name: entry.name,
-            fullPath: join(relativePath, entry.name),
-            type: isDirectory ? 'dir' : 'file',
-            contents,
-        });
-    }
-
-    return entries;
+  return entries;
 }
 
 /**
