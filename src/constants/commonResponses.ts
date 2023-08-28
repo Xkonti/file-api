97477d3d8c5e86ee15bbb6d19e137232061f8805
Xkonti <online.xkonti@gmail.com>
2023-08-28 19:06:57 -0400

    Allow binary stream file uploads

---
+++ src/constants/commonResponses.ts
@@ -0,0 +1,11 @@
+// Success
+export const fileUploadSuccess = 'File uploaded successfully';
+
+// Bad requests
+
+export const fileAlreadyExists = 'The file already exists';
+export const provideValidPath = 'You must provide a valid path to a file';
+export const unsupportedContentType = 'Unsupported content type';
+
+// Internal server errors
+export const uploadError = 'An error occurred while uploading the file';
--- src/endpoints/file/uploadFile.ts
+++ src/endpoints/file/uploadFile.ts
@@ -1,50 +1,86 @@
-import Elysia, {t} from 'elysia';
+import Elysia from 'elysia';
 import {isPathValid} from '../../utils/pathUtils';
 import {getConfig} from '../../utils/config';
 import {join} from 'path';
 import {getFile} from '../../utils/fileUtils';
+import {fileEmpty, getStaticError, isStaticError} from '../../constants/errorCodes';
+import {
+  fileAlreadyExists,
+  fileUploadSuccess,
+  provideValidPath,
+  unsupportedContentType,
+  uploadError,
+} from '../../constants/commonResponses';
 
 export function addUploadFileEndpoint(app: Elysia) {
-  return app.post(
-    'file',
-    async ({body: {file}, query, set}) => {
-      // Verify that the path is valid
-      let relativePath = query.path ? (query.path as string) : null;
-      if (!isPathValid(relativePath)) {
-        set.status = 400;
-        return 'You must provide a valid path to a file';
+  return app.post('file', async ({body, headers, query, set}) => {
+    // Verify the path
+    if (!query.path || typeof query.path !== 'string' || !isPathValid(query.path)) {
+      set.status = 400;
+      return provideValidPath;
+    }
+
+    // Get the full path to the file
+    const relativePath = query.path;
+    const filePath = join(getConfig().dataDir, relativePath);
+
+    // Check if the file should be overwritten if one exists already
+    const overwrite = (query.overwrite ?? 'false') === 'true';
+
+    // Check if file exists
+    const existingFile = await getFile(filePath);
+    if (existingFile && !overwrite) {
+      set.status = 409;
+      return fileAlreadyExists;
+    }
+
+    // Write the file
+    try {
+      // Default to invalid content type to prevent mishaps
+      const contentType = headers['content-type']?.split(';')[0] ?? '';
+
+      switch (contentType) {
+        // Binary stream
+        case 'application/octet-stream':
+          await writeFile(filePath, body as Blob);
+          break;
+
+        // Form data
+        case 'multipart/form-data':
+          const fileContents = (body as Record<string, any>).file as Blob;
+          await writeFile(filePath, fileContents);
+          break;
+
+        // Unsupported content type
+        default:
+          set.status = 400;
+          return unsupportedContentType;
       }
-      relativePath = relativePath as string;
 
-      // Get the full path to the file
-      const filePath = join(getConfig().dataDir, relativePath);
+      // Success
+      set.status = 201;
+      return fileUploadSuccess;
 
-      const overwrite = (query.overwrite ?? 'false') === 'true';
-
-      // Check if the file already exists
-      const existingFile = await getFile(filePath);
-      if (existingFile && !overwrite) {
-        set.status = 409;
-        return 'The file already exists';
-      }
-
-      // Write the file
-      try {
-        await Bun.write(filePath, file); // This overrites the file if it already exists
-        set.status = 201;
-        return 'File uploaded successfully';
-      } catch (error) {
-        set.status = 500;
-        if (error instanceof Error) {
-          return 'There was an error when uploading your file: ' + error.message;
+      // Handle errors
+    } catch (error) {
+      set.status = 500;
+      if (error instanceof Error) {
+        // If the file is empty, return a 400
+        if (isStaticError(error, fileEmpty)) {
+          set.status = 400;
+          return error.message;
         }
-        return 'There was an error when uploading your file';
+
+        return `${uploadError}: ${error.message}`;
       }
-    },
-    {
-      body: t.Object({
-        file: t.File(),
-      }),
-    },
-  );
+      return uploadError;
+    }
+  });
+}
+
+async function writeFile(filePath: string, fileContents: Blob) {
+  if (fileContents.size === 0) {
+    throw getStaticError(fileEmpty);
+  }
+  await Bun.write(filePath, fileContents);
 }
