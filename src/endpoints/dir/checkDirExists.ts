5c8ffc8f179a2982e6f7a42963fa549f8c60784a
Stéphane Changarnier <lansolo99@hotmail.fr>
2023-10-13 09:51:17 +0200

    Add checkDirExists endpoint (#38)
    This PR closes #13 
    Authored-by: Stéphane Changarnier <stephanechangarnier@Stephanes-MacBook-Pro-2.local>

---
+++ src/endpoints/dir/checkDirExists.ts
@@ -0,0 +1,31 @@
+import Elysia from 'elysia';
+import {validateRelativePath} from '../../utils/pathUtils';
+import {checkIfDirectoryExists} from '../../utils/directoryUtils';
+import {dirNotExistsMsg, provideValidPathDirMsg} from '../../constants/commonResponses';
+
+export function addCheckDirExistsEndpoint(app: Elysia) {
+  return app.get('dir/exists', async ({query, set}) => {
+    // Verify that the path is valid
+    const dirPathValidationResult = await validateRelativePath(query.path);
+    if (dirPathValidationResult.isErr()) {
+      set.status = 400;
+      return provideValidPathDirMsg;
+    }
+    // Get the full path to the directory
+    const {absolutePath} = dirPathValidationResult.value;
+
+    try {
+      // Get the directory
+      const dir = await checkIfDirectoryExists(absolutePath);
+      if (dir === false) {
+        set.status = 404;
+        return dirNotExistsMsg;
+      }
+      set.status = 204;
+      return '';
+    } catch (error) {
+      set.status = 400;
+      return provideValidPathDirMsg;
+    }
+  });
+}
